2f9bd96dcc3bc687db9da0759de78f94a248f902
Evan Guo <evanguo97@gmail.com>
2024-01-21 01:26:50 -0800

    add debug line

--- handler.py
+++ handler.py
@@ -1,8 +1,9 @@
 from domain.fx_service import FXService
 
 
-def handler(event, context):
+def lambda_handler(event, context):
     # Determine which handler to use based on the event
+    print(event, context)
     if event["path"] == "/cadusd-fx":
         fx_handler = FXService()
         return fx_handler.handle_request(event)
