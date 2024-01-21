5a1a81e22945615a8edb3ab68b284bf4797fa495
Evan Guo <evanguo97@gmail.com>
2024-01-21 02:27:20 -0800

    update handler

--- handler.py
+++ handler.py
@@ -5,4 +5,4 @@ def lambda_handler(event, context):
     # Determine which handler to use based on the event
     if event["fc.request_uri"] == "/cadusd-fx":
         fx_handler = FXService()
-        return fx_handler.handle_request(event)
+        return fx_handler.process_fx_data()
