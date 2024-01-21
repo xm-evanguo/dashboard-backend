ea828fd6f34add1444d114eb18385ebfc306bed2
Evan Guo <evanguo97@gmail.com>
2024-01-21 01:16:34 -0800

    fix: update FXservice name

--- handler.py
+++ handler.py
@@ -1,8 +1,8 @@
-from domain.fx_service import FXHandler
+from domain.fx_service import FXService
 
 
 def handler(event, context):
     # Determine which handler to use based on the event
     if event["path"] == "/cadusd-fx":
-        fx_handler = FXHandler()
+        fx_handler = FXService()
         return fx_handler.handle_request(event)
