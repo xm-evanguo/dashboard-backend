87080ecd43cf10c297b0522d2449b1cc96736029
Evan Guo <evanguo97@gmail.com>
2024-01-21 18:04:15 -0800

    update hanlder and yaml

--- handler.py
+++ handler.py
@@ -1,8 +1,24 @@
+import logging
+
 from domain.fx_service import FXService
 
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
-def lambda_handler(event, context):
+
+def lambda_handler(environ, start_response):
     # Determine which handler to use based on the event
-    if event["fc.request_uri"] == "/cadusd-fx":
+    request_uri = environ["fc.request_uri"]
+    if request_uri == "/cadusd-fx":
         fx_handler = FXService()
-        return fx_handler.process_fx_data()
+        result = fx_handler.process_fx_data()
+        logging.info(result)
+        status = "200 OK"
+        start_response(status)
+        return [result]
+    else:
+        status = "404 NOT FOUND"
+        start_response(status)
+        return []
